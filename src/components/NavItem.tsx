de020bfbd4238469e8dfd0d7fa628940bf09cb85
Eugene Solomonyk <solosoft2000@mail.ru>
2024-05-01 21:01:42 +0300

    refactor: react, react-icons

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,11 @@
-export const NavItem = ({ href, children }: { href: string, children: string}) => {
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+export const NavItem = ({ href, children }: { href: string, children: string | any[]}) => {
   return (
-    <li class="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+    <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
       <a href={href} className={`block px-3 py-2 rounded-md`}>
+      {/* <FontAwesomeIcon icon={faCoffee} /> */}
         {children}
       </a>
     </li>
