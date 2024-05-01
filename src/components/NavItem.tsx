10b592e8485a5bcc29a99c673fddf0553eefdce3
Eugene Solomonyk <solosoft2000@mail.ru>
2024-05-01 21:18:17 +0300

    styles

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,11 +1,7 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
 export const NavItem = ({ href, children }: { href: string, children: string | any[]}) => {
   return (
-    <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
-      <a href={href} className={`block px-3 py-2 rounded-md`}>
-      {/* <FontAwesomeIcon icon={faCoffee} /> */}
+    <li className="flex mt-6 items-center cursor-pointer hover:text-[#EC5252] duration-150">
+      <a href={href} className={`flex justify-between w-64 px-3 py-2 rounded-md`}>
         {children}
       </a>
     </li>
