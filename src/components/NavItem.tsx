54565d41c92d32e525fdadcc0b69248d805bce39
Eugene Solomonyk <solosoft2000@mail.ru>
2024-05-02 17:15:14 +0300

    start with main

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -16,13 +16,16 @@ export const NavItem = ({ href, isActive = false, children }: NavItemProps) => {
   return (
     <>
       {isActive ? (
-        <a className="flex py-2 justify-center items-center bg-gray-800 text-[#E3CBBD] duration-300">
+        <a className="flex py-2 justify-center items-center bg-[#A64B01] text-[#E3CBBD] duration-300">
           <Child />
         </a>
       ) : (
         <a
           href={href}
-          className="flex justify-center py-2 items-center bg-[#E3CBBD] text-gray-800 hover:cursor-pointer hover:text-[#E3CBBD] hover:bg-gray-500 duration-300"
+          className="flex justify-center py-2 items-center 
+              bg-gradient-to-l from-white from-10% to-[#E3CBBD] to-70% text-gray-800 
+              hover:cursor-pointer hover:text-white
+            hover:from-[#E3CBBD] hover:to-[#A64B01] duration-300"
         >
           <Child />
         </a>
