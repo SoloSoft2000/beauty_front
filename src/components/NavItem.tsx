31cf286a8424373d3c08d308effaa441874f8cba
Eugene Solomonyk <solosoft2000@mail.ru>
2024-05-02 07:18:27 +0300

    style for menu

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,9 +1,32 @@
-export const NavItem = ({ href, children }: { href: string, children: string | any[]}) => {
-  return (
-    <li className="flex mt-6 items-center cursor-pointer hover:text-[#EC5252] duration-150">
-      <a href={href} className={`flex justify-between w-64 px-3 py-2 rounded-md`}>
+type NavItemProps = {
+  href: string;
+  isActive?: boolean;
+  children: string | any[];
+};
+
+export const NavItem = ({ href, isActive = false, children }: NavItemProps) => {
+  const Child = () => {
+    return (
+      <div className={`flex justify-between w-64 px-3 py-2 rounded-md`}>
         {children}
-      </a>
-    </li>
-  )
-}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {isActive ? (
+        <a className="flex justify-center py-2 items-center bg-gray-800 text-[#E3CBBD] duration-300">
+          <Child />
+        </a>
+      ) : (
+        <a
+          href={href}
+          className="flex justify-center py-2 items-center bg-[#E3CBBD] text-gray-800 hover:cursor-pointer hover:text-[#E3CBBD] hover:bg-gray-500 duration-300"
+        >
+          <Child />
+        </a>
+      )}
+    </>
+  );
+};
